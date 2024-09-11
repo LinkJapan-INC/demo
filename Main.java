ce08c8631b3a4a456049c622c748a558d5d1cc37
sky5800 <develop.github.jp@gmail.com>
2024-09-11 14:38:43 +0900

    fixed code

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String key = System.getenv('FTP_KEY');
+        String key = System.getenv("FTP_KEY");
         System.out.println("##############");
         System.out.println(key);
     }
