f84dd5bb32acf86818c92647bfff07a4ecf44ec0
sky5800 <develop.github.jp@gmail.com>
2024-09-11 14:28:36 +0900

    fixed error

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String key = System.getenv('FTP_KEY')
-        System.out.println("##############")
-        System.out.println(key)
+        String key = System.getenv('FTP_KEY');
+        System.out.println("##############");
+        System.out.println(key);
     }
 }
