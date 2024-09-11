74aefdb05780ce20d85d5d61c707207010fb1abf
sky5800 <develop.github.jp@gmail.com>
2024-09-11 16:24:22 +0900

    code

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+
         String key = System.getenv("FTP_KEY");
         System.out.println(key);
+        Log.info(key)
     }
 }
